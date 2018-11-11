b16c223b45e121c73a7b2fac64d3c3f45f92ba9e
kemboicheru <kemboicheru@gmail.com>
2018-11-11 15:42:36 +0300

    Merge updates

--- app/aigent.py
+++ app/aigent.py
@@ -27,6 +27,33 @@ def post_to_api(data_type, recording_filename='', payload_array=[]):
 post_to_api('nouns', 'test_recording.wav', ["table", "car"])
 
 
+# process nouns/verbs
+@app.route('/tag', methods = ['POST'])
+def get_nouns_verbs(data_object):
+    data_object = {
+                    'filename':'../aigent.txt',
+                    'words':'Free listening exercises and activities for verbs listening tests from www.123 Listening.com   .  Many different audio downloads and many different worksheets that can be combined to be very simple for young learners or more difficult for older students.'
+                    }
+    fileName = '../aigent.txt' # data_object.file
+    # File = open(fileName) #open file
+    lines = data_object.words # File.read() #read all lines # data_object.words
+    sentences = nltk.sent_tokenize(lines) #tokenize sentences
+    nouns = [] #empty to array to hold all nouns
+    verbs = [] #empty array to hold all verbs
+    verb_tags = ['VB','VBD','VBG','VBN','VBP','VBZ']
+    noun_tags = ['NN','NNP','NNS','NNPS']
+
+    for sentence in sentences:
+         for word,pos in nltk.pos_tag(nltk.word_tokenize(str(sentence))):
+             if pos in noun_tags
+                 nouns.append(word)
+             elif pos in verb_tags:
+                 verbs.append(word)
+    print str(nouns)
+    print str(verbs)
+    # post_to_api('nouns', filename, nouns)
+    # post_to_api('verbs', filename, verbs)
+
 @app.route('/test_data',methods=['POST'] )
 def assess(arg):
     pass
